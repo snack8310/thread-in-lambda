666f059429f96f3bf7a4748fc87045e12da46e44
huisheng <huisheng@wacai.com>
2022-01-26 11:24:07 +0800

    add availableProcessors check

---
+++ src/main/java/com/snack/learning/threadinlambda/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.snack.learning.threadinlambda;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ThreadInLocalApplication.class);
+	}
+
+}
---
+++ src/main/java/com/snack/learning/threadinlambda/ThreadInLocalApplication.java
@@ -0,0 +1,13 @@
+package com.snack.learning.threadinlambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadInLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadInLocalApplication.class, args);
+	}
+
+}
